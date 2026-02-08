4de0b08f4502b852d5e4d8e7c4f08286f1fc66fd
Keith A. Bare II <105995606+keithbare2@users.noreply.github.com>
2026-02-08 14:39:29 -0500

    Add/update copyright and license notices
    The majority of cert-propagation is made available under a
    GPL-3.0-or-later license.

--- cert_receive/__init__.py
+++ cert_receive/__init__.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+
+# Copyright (C) 2022-2026 Keith Allen Bare II
+#
+# This file is part of cert-propagation.
+#
+# cert-propagation is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# cert-propagation is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with cert-propagation. If not, see <https://www.gnu.org/licenses/>.
+
 '''
 Python package implementing a cert_receive script that performs TLS/SSL
 certificate installation.
